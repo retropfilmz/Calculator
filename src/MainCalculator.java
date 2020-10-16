03b87e24c4d9bb05cae2fb9363e3eeb4d7147929
retropfilmz <61301467+retropfilmz@users.noreply.github.com>
2020-10-15 19:14:17 -0700

    Buttons added, but little to no functionality

--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -7,12 +7,38 @@ public class MainCalculator {
     private JPanel gui;
     private JTextField textInput1;
     private JButton displayButton;
+    private JButton button1;
+    private JButton button4;
+    private JButton button7;
+    private JButton button0;
+    private JButton buttonDecimal;
+    private JButton button2;
+    private JButton button5;
+    private JButton button8;
+    private JButton button9;
+    private JButton button6;
+    private JButton button3;
+    private JButton buttonEquals;
+    private JButton buttonAdd;
+    private JButton buttonSubtract;
+    private JButton buttonMultiply;
+    private JButton buttonDivide;
+    private JButton buttonExponent;
+    private JButton buttonNaturalLog;
+    private JButton logButton;
+    private JButton buttonSin;
+    private JButton buttonCos;
+    private JButton buttonTan;
+    private JButton buttonCot;
+    private JButton buttonRParenth;
+    private JButton buttonLParenth;
+    private JLabel result;
 
     public MainCalculator() {
         displayButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,textInput1.getText());
+                result.setText(textInput1.getText());
             }
         });
     }
